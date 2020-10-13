0846632cd3c8350de8a5c0817e9b8c7df2ebf576
Steelstone3 <70329398+Steelstone3@users.noreply.github.com>
2020-10-13 15:42:43 +0100

    Program.cs added

---
+++ StringCalculator/Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Tdd_Katas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

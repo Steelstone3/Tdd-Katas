0459b044f142867d30666ff33dd0ff5a473b9015
Skippy <steelstone3@gmail.com>
2025-01-07 12:50:51 +0000

    update

--- NinjaWars/Models/Ninja.cs
+++ NinjaWars/Models/Ninja.cs
@@ -1,11 +1,11 @@
 public class Ninja : INinja
 {
-    public uint Health { get; }
+    public uint Health { get; private set; } = 100;
 
     public int Attac() => 5;
 
     public void Protec(int damage)
     {
-        throw new System.NotImplementedException();
+
     }
 }
